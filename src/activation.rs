3b428db031efa99010e64cea2c1d5b410f10e650
Olle Fredriksson <fredriksson.olle@gmail.com>
2024-08-19 23:59:41 +0200

    Clean up traits

--- src/activation.rs
+++ src/activation.rs
@@ -1,19 +1,16 @@
-use std::ops::Neg;
-
 use ndarray::LinalgScalar;
 use num_traits::{Float, FromPrimitive};
 
-trait Activation<Signal> {
+pub trait Activation<Signal> {
     fn function(input: Signal) -> Signal;
     fn derivative(input: Signal) -> Signal;
 }
 
-struct Sigmoid;
-struct RelU;
+pub struct Sigmoid;
+pub struct RelU;
+pub struct LeakyRelU;
 
-struct LeakyRelU;
-
-impl<Signal: LinalgScalar + Neg<Output = Signal> + Float> Activation<Signal> for Sigmoid {
+impl<Signal: LinalgScalar + Float> Activation<Signal> for Sigmoid {
     fn function(input: Signal) -> Signal {
         Signal::one() / (Signal::one() + (-input).exp())
     }
@@ -24,7 +21,7 @@ impl<Signal: LinalgScalar + Neg<Output = Signal> + Float> Activation<Signal> for
     }
 }
 
-impl<Signal: LinalgScalar + Ord> Activation<Signal> for RelU {
+impl<Signal: LinalgScalar + Float> Activation<Signal> for RelU {
     fn function(input: Signal) -> Signal {
         input.max(Signal::zero())
     }
@@ -38,7 +35,7 @@ impl<Signal: LinalgScalar + Ord> Activation<Signal> for RelU {
     }
 }
 
-impl<Signal: LinalgScalar + Ord + FromPrimitive> Activation<Signal> for LeakyRelU {
+impl<Signal: LinalgScalar + Float + FromPrimitive> Activation<Signal> for LeakyRelU {
     fn function(input: Signal) -> Signal {
         if input > Signal::zero() {
             input
